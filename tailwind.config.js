0cab64a6c788b5e76157af7bc9271eeca6c7d4f4
vaaakoo <vaaakoo@gmail.com>
2022-12-12 17:16:45 +0400

    all file their folders #1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        lightGray: "#727A82", // Gray text color
-        lightGrayButton: "#F3F3F4", //Gray buttons
-        midGray: "#F5F5F7", // Background color
-        borderGray: "#EAEAEB", // Border color
         greenButton: "#1A8917", // Green buttons
-        darkButton: "#323329", // Dark buttons
-        greenText: "#327F37", // Green text
-        lightGreen: "#1A8917", // Light green text for active tabs
-        activeGreen: "#2FA837",
-        lightGreenIcons: "#F2F6EC",
-        borderBlue: "#2F69FF",
-        backgroundBlue: "#2F69FF0A",
         lightBlack: "#292D33", // Main black color
-        progress: "#2f69ff0a", //Color of progress bar
         grayBorder: "#E6E6E6", //Border Color for dropdown item's container
-        grayHover: "#F5F5F5", //Hover background color for dropdown items
         blackText: "#1B2127", //Black text for dropdown
         lightDropBlack: "#6F7680", //Light black text for dropdown on hover
       },
